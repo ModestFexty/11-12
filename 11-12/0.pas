575c16b71cdbab3f8ba1a39c2daf71a340a70aa9
ModestFexty <117993287+ModestFexty@users.noreply.github.com>
2022-11-29 16:43:10 +0300

    Add files via upload

---
+++ 11-12/0.pas
@@ -0,0 +1,12 @@
+﻿uses GraphABC;
+begin
+  MoveTo(100,100);
+  LineTo(100,150);
+  LineTo(150,150);
+  LineTo(150,100);
+  LineTo(100,100);
+  MoveTo(400,100);
+  LineTo(500,300);
+  LineTo(300,300);
+  LineTo(400,100);
+end.
---
+++ 11-12/1.pas
@@ -0,0 +1,15 @@
+﻿uses GraphABC;
+begin
+  setpenwidth(2);
+  line(300,200,500,300);
+  line(300,200,100,300);
+  line(100,300,500,300);
+  line(100,300,300,400);
+  line(300,400,500,300);
+  floodfill(300,250,clblue);
+  floodfill(350,350,cllime);
+  circle(50,300,50);
+  floodfill(70,320,clred);
+  circle(550,300,50);
+  floodfill(520,300,clyellow);
+end.
